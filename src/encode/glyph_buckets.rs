33ce76342b25c38b881b4534363621da16a228f1
Nick Sanders <cogciprocate@gmail.com>
2016-03-21 19:29:16 -0700

    Add tmp_data to repo.

--- src/encode/glyph_buckets.rs
+++ src/encode/glyph_buckets.rs
@@ -14,10 +14,10 @@ pub struct GlyphBuckets {
 impl GlyphBuckets {
     pub fn new() -> GlyphBuckets {
         let bucket_count = 10;
-        let label_file = Search::ParentsThenKids(3, 3).for_folder("data").unwrap()
-            .join("train-labels-idx1-ubyte");
-        let image_file = Search::ParentsThenKids(3, 3).for_folder("data").unwrap()
-            .join("train-images-idx3-ubyte");
+        let label_file = Search::ParentsThenKids(3, 3).for_folder("tmp_data")
+            .expect("GlyphBuckets::new(): 'label_file'").join("train-labels-idx1-ubyte");
+        let image_file = Search::ParentsThenKids(3, 3).for_folder("tmp_data")
+            .expect("GlyphBuckets::new(): 'image_file'").join("train-images-idx3-ubyte");
         let labels = IdxData::new(label_file, false);
         let mut images = IdxData::new(image_file, true);
 
