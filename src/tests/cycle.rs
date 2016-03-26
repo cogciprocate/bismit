dfcca3e69fe0187599bff02ad0401abfda1ff812
Nick Sanders <cogciprocate@gmail.com>
2016-03-26 09:37:41 -0700

    Enable ocl debugging features.

--- src/tests/cycle.rs
+++ src/tests/cycle.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables, unused_mut)]
+
 use tests::testbed_vibi;
 // use cortex::{Cortex};
 
@@ -5,7 +7,7 @@ use tests::testbed_vibi;
 fn cycle() {
 	let mut cortex = testbed_vibi::new_cortex();
 	let area_name = "v1".to_owned();
-	let mut cycle_iters = 2000;
+	let mut cycle_iters = 20;
 
 	for _ in 0..cycle_iters {
 		cortex.cycle();
